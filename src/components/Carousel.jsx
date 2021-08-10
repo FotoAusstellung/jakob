7534c3cca89e1ebf60555be8981b609b4e622ac7
Effiti <EffitiDEV@gmail.com>
2021-08-10 15:24:11 +0200

    initital commit v2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,11 @@
 import React, { Component, setState } from "react";
 import { ImageField } from "./ImageField";
 import { ArrowButton } from "./ArrowButton";
-import { Container } from '@material-ui/core';
-
+import { Button } from "@material-ui/core"
 
 export class Carousel extends Component {
   state = { Image: 1 };
+
   constructor(props){
 	  super(props);
 	  this.nextImage = this.nextImage.bind(this);
@@ -19,17 +19,22 @@ export class Carousel extends Component {
         <ArrowButton direction={1} clickfunction={this.nextImage} />
 		<p>{this.state.Image}</p>
 
+
       </div>
     );
   }
   nextImage() {
     this.setState(((prevState) => {
-      return { Image: prevState.Image >= 88 ? 1 : prevState.Image + 1 };
+
+
+	  return { Image: prevState.Image === 54 ? 1 : prevState.Image + 1 };
     }));
   }
   previousImage() {
     this.setState(((prevState) => {
-      return { Image: prevState.Image <= 1 ? 88 : prevState.Image - 1 };
+
+	  return { Image: prevState.Image <= 1 ? 54 : prevState.Image - 1 };
     }));
   }
+  
 }
